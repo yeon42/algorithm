ededbb1decf2c034bc92db787ec2508c326512d5
yeon42 <jungyune22@gmail.com>
2022-05-13 11:09:03 +0900

    삽입 정렬

---
+++ Flipped/06_Sorting/6-3.py
@@ -0,0 +1,16 @@
+# 6-3. 삽입 정렬 소스코드
+
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+for i in range(1, len(array)):
+    for j in range(i, 0, -1):
+        if array[j] < array[j-1]: # 한 칸씩 왼쪽으로 이동
+            array[j], array[j-1] = array[j-1], array[j]
+        else: # 자기보다 작은 데이터를 만나면 그 위치에서 멈춤
+            break
+
+print(array)
+
+
+# 시간 복잡도: O(N^2)
+# 이미 정렬되어 있는 경우 삽입 정렬 시간 복잡도: O(N)
